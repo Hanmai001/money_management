54df0e68b4eb26dbdcd1099848d99bf245941d21
Hanmai001 <88010632+Hanmai001@users.noreply.github.com>
2022-08-27 10:58:09 +0700

    setup client

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Home.module.scss';
+
+function Home() {
+    return (
+      <div className="container-fluid">
+        
+      </div>
+    );
+}
+
+export default Home;
