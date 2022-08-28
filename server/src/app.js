3216e03d2fcf6d780396c1580a0f766fbe3de5d5
NTL <loicho0167277@gmail.com>
2022-08-28 10:14:56 +0700

    update create server web and connect serverdb

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const passport = require("passport");
+const session = require('express-session')      // dung de luu truu cookie https://www.npmjs.com/package/express-session
+
+const bodyParser = require("body-parser");
 require('dotenv').config()
 
 
 const db = require("./configs/db/index");
+
 const app = express();
+
+//todo setting server
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(session({           // tao goi session cho may chu trang web
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());     // khoi tao 
+app.use(passport.session());        // yeu cau he thong su dung passport de dua session vao 
+app.use(express.json());
 
 //todo connet db
 db.connect();
